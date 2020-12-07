2532c15a7922be522f47d68495e5889e1603718e
kali rajan <kalirajan619@gmail.com>
2020-12-07 20:50:04 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Node JS APP");
+});
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
+    console.log("Wazzappppp");
+});
